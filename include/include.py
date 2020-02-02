bd09a4b02c40b6c65f7ad957b60e21b705dfa916
mrange <marten@atomize>
2020-02-02 20:17:19 +0100

    kind of works

--- include/include.py
+++ include/include.py
@@ -1,21 +1,24 @@
-import urllib.request
 import re
+import urllib.parse
+import urllib.request
 
+class UriInclude:
+  def __init__(self, path):
+    self.path = path
+
+  def uri(self):
+    return self.path
+
+def include(path):
+  return UriInclude(root_url + path)
 
 root_url      = "https://raw.githubusercontent.com/mrange/WebSourceInclude/master/tests/scala_include/"
 target_file   = "../tests/scala_app/src/main/scala/scala_app/WebSourceInclude.scala"
 root_package  = "scala_app"
 
-class Include:
-  def __init__(self, path):
-    self.path = path
-
-  def uri(self):
-    return root_url + self.path
-
-
 includes = [
-    Include("streams/PushStream.scala")
+    include("streams/PushStream.scala"),
+    include("logs/Log.scala"),
   ]
 
 regex_include = re.compile("""^\s*//\s*###\s*INCLUDE:\s*(?P<path>\S+)\s*$""")
@@ -29,18 +32,20 @@ def generate_target_file():
       ""
     ]
 
-  if current:
+  while current:
     for include in current:
       uri = include.uri()
 
+      print("Processing: %s" % uri)
+
       if uri in seen:
-        print("Skipping (already seen): %s" % uri)
+        print("  Skipping (already seen): %s" % uri)
         lines.append("")
         lines.append("// @@@ SKIPPED (already seen): %s" % uri)
         lines.append("")
       else:
         seen.add(uri)
-        print("Downloading: %s" % uri)
+        print("  Downloading: %s" % uri)
         lines.append("")
         lines.append("// @@@ BEGIN: %s" % uri)
         lines.append("")
@@ -49,6 +54,13 @@ def generate_target_file():
           content = response.read().decode("utf-8")
           for line in content.splitlines():
             lines.append(line)
+            m = regex_include.match(line)
+            if m:
+              path = m.group("path")
+              inc = urllib.parse.urljoin(uri, path)
+              print("  Found include: %s" % inc)
+              lines.append("// @@@ INCLUDE: %s" % inc)
+              nxt.append(UriInclude(inc))
 
         lines.append("")
         lines.append("// @@@ END: %s" % uri)
