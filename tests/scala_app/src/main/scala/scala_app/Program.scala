5a41dd492a04af0b3adcee9230f85001966d7617
mrange <marten@atomize>
2020-02-02 20:24:44 +0100

    Updated

--- tests/scala_app/src/main/scala/scala_app/Program.scala
+++ tests/scala_app/src/main/scala/scala_app/Program.scala
@@ -1,9 +1,11 @@
 package scala_app
 
+import web_source_include.logs._
 import web_source_include.streams._
 
 object Program {
   def main(args: Array[String]): Unit = {
+    val l : Log = null
     val e = PushStream.empty
 
     println("Hello")
--- tests/scala_app/src/main/scala/scala_app/WebSourceInclude.scala
+++ tests/scala_app/src/main/scala/scala_app/WebSourceInclude.scala
@@ -5,20 +5,21 @@ package scala_app
 
 package web_source_include.streams {
 
-// ### INCLUDE: ../logs/Log.scala
+  // ### INCLUDE: ../logs/Log.scala
 // @@@ INCLUDE: https://raw.githubusercontent.com/mrange/WebSourceInclude/master/tests/scala_include/logs/Log.scala
 
-trait PushStream[+T] {
-  def build(r: T => Boolean): Boolean
-}
-
-object PushStream {
-  def empty = new PushStream[Nothing] {
-    def build(r: Nothing => Boolean): Boolean = true
+  trait PushStream[+T] {
+    def build(r: T => Boolean): Boolean
   }
-}
+
+  object PushStream {
+    def empty = new PushStream[Nothing] {
+      def build(r: Nothing => Boolean): Boolean = true
+    }
+  }
 
 }
+
 // @@@ END: https://raw.githubusercontent.com/mrange/WebSourceInclude/master/tests/scala_include/streams/PushStream.scala
 
 
@@ -26,9 +27,9 @@ object PushStream {
 
 package web_source_include.logs {
 
-trait Log {
-  def log(m: String): Unit
-}
+  trait Log {
+    def log(m: String): Unit
+  }
 
 }
 
